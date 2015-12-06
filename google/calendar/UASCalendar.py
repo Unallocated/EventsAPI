81f7bcecf308efa7b1f2d088eafd5a8c0de84a83
main <main@new-host-4.home>
2015-12-05 21:55:42 -0500

    Merge conflict fix

--- google/calendar/UASCalendar.py
+++ google/calendar/UASCalendar.py
@@ -6,14 +6,23 @@ class UASCalendar:
     
     def create_event(self, event_body, notify=False):
         event = self.__service.events().insert(calendarId=self.__calendar_id, body=event_body.obj).execute()
+        print event.get('htmlLink')
         return event
 
-    def delete_event(event_id, notify=False):
+    def delete_event(self, event_id, notify=False):
         self.__service.events().delete(calendarId=self.__calendar_id, eventId=event_id, sendNotifications=notify)
 
-    def delete_events(event_id_list, notify=False):
+    def delete_events(self, event_id_list, notify=False):
         for eventt_id in event_id_list:
             self.__service.events().delete(calendarId=self.__calendar_id, eventId=event_id, sendNotifications=notify)
+    
+    def list_events(self):
+        pageToken = None
+        resp = self.__service.events().list(calendarId=self.__calendar_id, pageToken=pageToken).execute()
+        for i in resp['items']:
+            if 'end' in i and 'timeZone' in i['end'] and 'description' in i and 'htmlLink' in i:
+                print '%s :::: %s :::: %s :::: %s :::: %s to %s' % (i['end']['timeZone'], i['description'], i['created'], i['htmlLink'], i['start']['dateTime'], i['end']['dateTime'])
+
 
     def update_event(self, event_id, event_body, notify=False):
         return self.__service.events().update(calendarId=self.__calendar_id, eventId=event_id, body=event_body, sendNotifications=notify).execute()
@@ -27,10 +36,11 @@ class EventBody:
 			'location' : '501 Shaw Ct',
 			'description' : description,
 			'start' : {
-        'dateTime': startTime
+                            'dateTime': startTime
 			},
 			'end' : {
-        'dateTime': endTime
+                            'dateTime': endTime
+
 			}
 		}
 
@@ -60,20 +70,24 @@ class EventBody:
 
 
 if __name__ == "__main__":
+    import datetime
+
+    currentDate = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     a = {
             "end": {
-                'dateTime': "2015-12-05T23:00:00",
-                "timeZone": 'UTC'
+                'dateTime': currentDate,
+                "timeZone": 'EST'
             },
             "start": {
-                'dateTime': "2015-12-05T18:00:00",
-                "timeZone": 'UTC'
+                'dateTime': currentDate,
+                "timeZone": 'EST'
             },
-            "calendarId": 'uas.events.tester@gmail.com',
+            "calendarId": 'uaas.events.tester@gmail.com',
             "summary": 'moo cow',
             "description": 'just a test',
             "location": '1234'
         };
 
+    e = EventBody(summary='moo cow summary', description='moo cow description', startTime='2015-12-05T11:00:00', endTime='2015-12-05T12:00:00')
     api = UASCalendar()
-    api.create_event(a)
+    api.create_event(e)
