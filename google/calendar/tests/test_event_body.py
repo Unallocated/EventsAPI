84b46322adc1590fe5b969716660be25a63c7471
Chris W Jones <chris@christopherjones.us>
2015-12-06 10:01:02 -0500

    Adds more tests
    I added tests for `EventBody.getSummary` and `EventBody.getDescription`.

--- google/calendar/tests/test_event_body.py
+++ google/calendar/tests/test_event_body.py
@@ -2,22 +2,38 @@ import unittest
 from UASCalendar import EventBody
 
 class TestEventBody(unittest.TestCase):
+  def setUp(self):
+    self.eb = EventBody("test summary", "test desc", "2015-12-05T12:00:00-06:00", "2015-12-05T12:01:00-06:00")
+  
   def test_create_event(self):
-    #setup
     #exercise
-    eb = EventBody("test summary", "test desc", "2015-12-05T12:00:00-06:00", "2015-12-05T12:01:00-06:00")
+    event = self.eb
     
     #verify
-    self.assertIsInstance(eb, EventBody)
-
-    #teardown
-
+    self.assertIsInstance(event, EventBody)
+    
   def test_get_summary(self):
-    pass
     #setup
+    event = self.eb
+    
     #exercise
+    summary = event.getSummary()
+    
     #verify
+    self.assertEqual(summary, "test summary")
+    
     #teardown
+    
+  def test_get_description(self):
+    #setup
+    event = self.eb
+    
+    #exercise
+    description = event.getDescription()
+    
+    #verify
+    self.assertEquals(description, "test desc")
+    
 
 if __name__ == '__main__':
   unittest.main()
