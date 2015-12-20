fa47455e9b068b8335cb26b4b3a0af112e00f9c1
Chris W Jones <chris@christopherjones.us>
2015-12-20 13:43:55 -0500

    Adds location parameter and checks recurrence
    I made the location a parameter when creating an EventBody object since
    it may not always be at UAS.  I also wrote some code to check that the
    value passed as `recurrence` makes sense.  Finally, I wrote more tests.
    
    Issue: #6

--- google/calendar/tests/test_event_body.py
+++ google/calendar/tests/test_event_body.py
@@ -3,7 +3,7 @@ from UASCalendar import EventBody
 
 class TestEventBody(unittest.TestCase):
   def setUp(self):
-    self.eb = EventBody("test summary", "test desc", "2015-12-05T12:00:00-06:00", "2015-12-05T12:01:00-06:00")
+    self.eb = EventBody(summary="test summary", description="test desc", startTime="2015-12-05 12:00:00-06:00", endTime="2015-12-05 12:01:00-06:00", recurrences="weekly")
   
   def test_create_event(self):
     #exercise
@@ -20,7 +20,7 @@ class TestEventBody(unittest.TestCase):
     summary = event.getSummary()
     
     #verify
-    self.assertEqual(summary, "test summary")
+    self.assertEquals(summary, "test summary")
     
     #teardown
     
@@ -33,7 +33,50 @@ class TestEventBody(unittest.TestCase):
     
     #verify
     self.assertEquals(description, "test desc")
-    
+
+  def test_get_start_time(self):
+    #setup
+    event = self.eb
+
+    #exercise
+    start_time = event.getStartTime()
+
+    #verify
+    self.assertEquals(start_time, "2015-12-05T12:00:00-06:00")
+
+  def test_get_end_time(self):
+    #setup
+    event = self.eb
+
+    #exercise
+    end_time = event.getEndTime()
+
+    #verify
+    self.assertEquals(end_time, "2015-12-05T12:01:00-06:00")
+
+  def test_has_recurrences(self):
+    #setup
+    event = self.eb
+
+    #exercise
+    recurrence = event.hasrecurrences()
+
+    #verify
+    self.assertTrue(recurrence)
+
+  def test_get_recurrences(self):
+    #setup
+    event = self.eb
+
+    #exercise
+    recurrence = event.getrecurrence()
+
+    #verify
+    self.assertEquals(recurrence, 'RRULE:FREQ=WEEKLY;')
+
+  def test_raises_error_on_bad_recurrence(self):
+    with self.assertRaises(ValueError):
+      EventBody(summary="test summary", description="test desc", startTime="2015-12-05 12:00:00-06:00", endTime="2015-12-05 12:01:00-06:00", recurrences="wrong")
 
 if __name__ == '__main__':
   unittest.main()
